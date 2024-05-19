70cb103dd9c9d2570c4b456d4068d75424f523f2
İsmail Can Karataş <ismailcankaratasss@gmail.com>
2024-05-19 21:55:30 +0300

    MediatR added and ProductController created

---
+++ ECommerce.Api/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using ECommerce.Application.Features.Products.Queries.GetAllProducts;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMediator mediator;
+
+        public ProductController(IMediator mediator1)
+        {
+            this.mediator = mediator1;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var response = await mediator.Send(new GetAllProductsQueryRequest());
+            return Ok(response);
+        }
+    }
+}
